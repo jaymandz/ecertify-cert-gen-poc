c88e2c00c9e4f0741add9780a092296ee26c02e2
Jvergel Mandane <jvergel.mandane@asti.dost.gov.ph>
2024-05-22 09:36:32 +0800

    Added a database connection and MySQL support

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
+from os import getenv
+
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 application = Flask(__name__)
+application.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
+
+db = SQLAlchemy(application)
+migrate = Migrate(application, db)
 
 @application.get('/')
 def index():
