4593b68d6cd2731703d202765c1ccaab957e3b5a
Jvergel Mandane <jvergel.mandane@asti.dost.gov.ph>
2024-05-24 17:57:34 +0800

    Certificate type creation form now works

--- blueprints/certificate_types.py
+++ blueprints/certificate_types.py
@@ -19,6 +19,39 @@ def create():
         title='Create a certificate type',
     )
 
+@certificate_types_blueprint.post('/')
+def store():
+    t = CertificateType()
+    t.name = request.form['name']
+
+    db.session.add(t)
+
+    field_ids = [ int(id) for id in request.form.getlist('field-ids') ]
+    field_descriptions = request.form.getlist('field-descriptions')
+    field_value_types = request.form.getlist('field-value-types')
+    field_required_flags = request.form.getlist('field-required-flags')
+
+    for index, id in enumerate(field_ids):
+        if id == 0:
+            f = CertificateTypeField()
+            f.description = field_descriptions[index]
+            f.value_type = field_value_types[index]
+
+            try: f.is_required = field_required_flags[0] == true
+            except IndexError: f.is_required = False
+
+            db.session.add(f)
+        else:
+            f = db.get_or_404(CertificateTypeField, id)
+            f.description = field_descriptions[index]
+            f.value_type = field_value_types[index]
+
+            try: f.is_required = field_required_flags[0] == true
+            except IndexError: f.is_required = False
+    
+    db.session.commit()
+    return redirect(url_for('certificate_types.index'))
+
 @certificate_types_blueprint.get('/<int:id>')
 def show(id):
     t = db.get_or_404(CertificateType, id)
