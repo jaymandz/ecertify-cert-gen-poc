602516201a8d8d087ec800bc4e9ae67ba37147a5
Jvergel Mandane <jvergel.mandane@asti.dost.gov.ph>
2024-06-20 11:26:37 +0800

    Changed type of content column in Template model and re-generated all migrations

--- models.py
+++ models.py
@@ -27,5 +27,6 @@ class Template(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     certificate_type = db.Column(db.ForeignKey(CertificateType.id))
     name = db.Column(db.String(255))
-    content = db.Column(LONGTEXT)
-    # ^ Hopefully I can find a way to create a LONGTEXT in PostgreSQL
+    content = db.Column(
+        db.Text().with_variant(LONGTEXT, 'mysql', 'mariadb')
+    )
