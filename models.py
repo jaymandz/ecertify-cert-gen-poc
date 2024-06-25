c8403b5329fe47026c99d417b5680af6621cfd2b
Jvergel Mandane <jvergel.mandane@asti.dost.gov.ph>
2024-06-25 17:58:03 +0800

    Many model columns have been specified as not nullable

--- models.py
+++ models.py
@@ -11,37 +11,44 @@ class FieldValueTypeEnum(Enum):
 
 class CertificateType(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
+    name = db.Column(db.String(255), nullable=False)
 
     templates = db.relationship('Template')
     fields = db.relationship('CertificateTypeField')
 
 class CertificateTypeField(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    certificate_type_id = db.Column(db.ForeignKey(CertificateType.id))
-    name = db.Column(db.String(255))
-    description = db.Column(db.String(255))
-    value_type = db.Column(db.Enum(FieldValueTypeEnum))
-    is_required = db.Column(db.Boolean())
+    certificate_type_id = db.Column(
+        db.ForeignKey(CertificateType.id),
+        nullable=False,
+    )
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    value_type = db.Column(db.Enum(FieldValueTypeEnum), nullable=False)
+    is_required = db.Column(db.Boolean(), nullable=False)
 
     certificate_type = db.relationship('CertificateType')
 
 class Template(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    certificate_type_id = db.Column(db.ForeignKey(CertificateType.id))
-    name = db.Column(db.String(255))
+    certificate_type_id = db.Column(
+        db.ForeignKey(CertificateType.id),
+        nullable=False,
+    )
+    name = db.Column(db.String(255), nullable=False)
     content = db.Column(
-        db.Text().with_variant(LONGTEXT, 'mysql', 'mariadb')
+        db.Text().with_variant(LONGTEXT, 'mysql', 'mariadb'),
+        nullable=False,
     )
 
     certificate_type = db.relationship('CertificateType')
 
 class Certificate(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    template_id = db.Column(db.ForeignKey(Template.id))
-    name = db.Column(db.String(255))
-    issuance_date = db.Column(db.Date())
-    issuance_locale = db.Column(db.String(255))
+    template_id = db.Column(db.ForeignKey(Template.id), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    issuance_date = db.Column(db.Date(), nullable=False)
+    issuance_locale = db.Column(db.String(255), nullable=False)
 
     template = db.relationship('Template')
     fields = db.relationship('CertificateField')
@@ -49,9 +56,10 @@ class Certificate(db.Model):
 
 class CertificateField(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    certificate_id = db.Column(db.ForeignKey(Certificate.id))
+    certificate_id = db.Column(db.ForeignKey(Certificate.id), nullable=False)
     certificate_type_field_id = db.Column(
-        db.ForeignKey(CertificateTypeField.id)
+        db.ForeignKey(CertificateTypeField.id),
+        nullable=False,
     )
     value = db.Column(db.String(255))
 
