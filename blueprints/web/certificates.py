bd8ae36ce1d42609ee21d4a666fc4eb72a7fb757
Jvergel Mandane <jvergel.mandane@asti.dost.gov.ph>
2024-06-25 13:36:03 +0800

    Certificate creation now works

--- blueprints/web/certificates.py
+++ blueprints/web/certificates.py
@@ -1,7 +1,16 @@
-from flask import Blueprint, render_template, request
+import re
+
+from flask import Blueprint, redirect, render_template, request, url_for
 from sqlalchemy import func
 
-from models import Certificate, CertificateType, db
+from models import (
+    Certificate,
+    CertificateField,
+    CertificateType,
+    CertificateTypeField,
+    Template,
+    db,
+)
 
 certificates_blueprint = Blueprint('certificates', __name__)
 
@@ -30,12 +39,37 @@ def create():
 
 @certificates_blueprint.post('/')
 def store():
-    return request.form
     c = Certificate()
+    c.template_id = int(request.form['template_id'])
     c.name = request.form['name']
     c.issuance_date = request.form['issuance_date']
     c.issuance_locale = request.form['issuance_locale']
 
     db.session.add(c)
+
+    for field_name in request.form:
+        if not re.match('field__', field_name): continue
+
+        ctype_id = db.session.execute(
+            db.select(Template).where(Template.id==c.template_id)
+        ).scalar_one().certificate_type_id
+
+        tf = db.session.execute(
+            db.select(CertificateTypeField). \
+              where(CertificateTypeField.certificate_type_id == ctype_id). \
+              where(CertificateTypeField.name == field_name[7:])
+        ).scalar_one()
+
+        cf = CertificateField()
+        cf.certificate_id = c.id
+        cf.certificate_type_field_id = tf.id
+        cf.value = request.form[field_name]
+        db.session.add(cf)
+
     db.session.commit()
     return redirect(url_for('certificates.index'))
+
+@certificates_blueprint.get('/<int:id>')
+def show(id):
+    c = db.get_or_404(Certificate, id)
+    return 'Under construction'
