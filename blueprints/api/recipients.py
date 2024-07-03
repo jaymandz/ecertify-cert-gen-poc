5a44165622c425271c2400b20cad698bd2485fee
Jvergel Mandane <jvergel.mandane@asti.dost.gov.ph>
2024-07-03 13:18:24 +0800

    ZIP files containing recipient copies of a certificate can now be downloaded

---
+++ blueprints/api/recipients.py
@@ -0,0 +1,20 @@
+from flask import Blueprint
+
+from models import Recipient, db
+
+recipients_blueprint = Blueprint('recipients', __name__)
+
+def dict_from_scalar(s): return {
+    'token': s.token,
+    'last_name': s.last_name,
+    'first_name': s.first_name,
+    'middle_name': s.middle_name,
+    'honorific': s.honorific,
+    'suffix': s.suffix,
+    'organization': s.organization,
+    'address': s.address,
+}
+
+@recipients_blueprint.post('/')
+def store():
+    return 'Under construction'
